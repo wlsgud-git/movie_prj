90123da8a9522bbd96c76d23f361e4bf1521b9d3
seoyk <seobear95@gmail.com>
2025-11-17 15:32:14 +0900

    홍길동 17 예매 코드 추가

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -2,10 +2,10 @@ package movie;
 
 public class Reservation {
 
-    public boolean reserve11(Movie movie, int count) {
+    public boolean reserve_17(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
-            System.out.println("서연경");
+            System.out.println("홍길동");
             return true;
         } else {
             System.out.println("❌ 예매 실패: 좌석 부족");
