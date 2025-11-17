b3bef2f31e6486c647c0e3d65b2b5bb47e0bed78
hyunjun <hyunjun041002@gmail.com>
2025-11-17 16:26:54 +0900

    Roll11 기능 구현해서 완료. 최종커밋

--- src/movie/Roll11.java
+++ src/movie/Roll11.java
@@ -1,7 +1,7 @@
 package movie;
 
-public class Roll11 {
-    public void Roll11() {
-        System.out.println("Roll11");
+public class Login11 {
+    public void doLogin11() {
+        System.out.println(" 서연경 로그인 성공!");
     }
 }
